48b4cbabc62e424fe3e49969cc0cd7e309128e9e
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-11 21:42:12 +0530

    update product card to show show info from params

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'i.ibb.co',
+        hostname: 'files.edgestore.dev',
         port: '',
         pathname: '/**',
       },
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,27 +8,30 @@ import { Button } from "./ui/button"
 import { Avatar,AvatarImage,AvatarFallback } from "./ui/avatar"
 import { cn } from "@/lib/utils"
 
-export default function ProductCard({hideCart,disableCard}) {
+export default function ProductCard({hideCart,disableCard,product}) {
+  const {name,price,images,description,seller} = product;
   return(
     <Card className="min-w-[280px] hover:shadow-2xl relative">
       <div className={cn(disableCard?"":"hidden","bg-slate-400/40 h-full w-full absolute rounded-sm z-50")}/>
       <div className={cn(hideCart ? "h-[403px]" : "h-[410px]")}>
-        <CardContent>
-          <Image src="https://i.ibb.co/D46XN9S/Algorithms-clrs.jpg" alt="Product image" width={200} height={200} className={cn(disableCard?"grayscale":"","overflow-hidden border-b-2 my-2 mx-auto")}/>
-          <h1 className="font-semibold">Agorithms CLRS</h1>
-          <span className="text-slate-500 text-sm">Small discription </span>
-          <div className={cn(hideCart ? "hidden" : "flex","gap-1 items-center mt-1")}>
-            <Avatar className="cursor-pointer hover:drop-shadow-2xl w-4 h-4">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <span className="text-slate-500 text-sm">Seller name</span>
-          </div>
-          <div className="flex justify-between mt-2 items-center">
-            <div className='text-sm p-2 border-2 rounded-md'>500₹</div>
-            <Button className={cn(hideCart?'hidden':"",'rounded-md text-md')}>
-              <BsCartPlus/>
-            </Button>
+        <CardContent className="relative h-full w-full flex flex-col items-center">
+          <Image src={images[0].url} alt="Product image" width={200} height={200} className={cn(disableCard?"grayscale":"","overflow-hidden my-auto")}/>
+          <div className="mx-auto border-t-2">
+            <h1 className="font-semibold pt-2">{name}</h1>
+            <span className="text-slate-500 text-sm text-wrap">{description.substring(0,35)}</span>
+            <div className={cn(hideCart ? "hidden" : "flex","gap-1 items-center mt-1")}>
+              <Avatar className="cursor-pointer hover:drop-shadow-2xl w-4 h-4">
+                <AvatarImage src={seller.profile_pic} alt="@shadcn" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <span className="text-slate-500 text-sm">{seller.username}</span>
+            </div>
+            <div className="flex justify-between mt-2 items-center">
+              <div className='text-sm p-2 border-2 rounded-md'>{price}₹</div>
+              <Button className={cn(hideCart?'hidden':"",'rounded-md text-md')}>
+                <BsCartPlus/>
+              </Button>
+            </div>
           </div>
         </CardContent>
       </div>
