488bc976b28ca37994f32696a421c00f84de5d18
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-12 09:13:57 +0530

    Install react-query dev tools

--- src/providers/QueryProvider.js
+++ src/providers/QueryProvider.js
@@ -4,12 +4,14 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { useState } from 'react'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const QueryProvider = ({children}) => {
   const [queryClient] = useState(() => new QueryClient())
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+    <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
