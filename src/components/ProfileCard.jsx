f2cf32c8f9c2050a09974c56575fd09d292124c4
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-14 21:29:40 +0530

    Fix user object null checks in ProfileCard component

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -15,20 +15,20 @@ export default function ProfileCard({user}) {
             <div className = "border rounded-lg shadow-md sm:w-[400px] w-full flex flex-col items-center p-8 gap-3">
                 <div className = "">
                     <Avatar className = "w-30 h-30">
-                        <AvatarImage src={user.profile_pic} />
+                        <AvatarImage src={user?.profile_pic} />
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar> 
                 </div>
                 <div className = "flex flex-col items-start w-full">
-                    <h1 className = "text-3xl font-semibold">{user.name}</h1>
-                    <p className = "text-gray-500 italic">@{user.username}</p>
+                    <h1 className = "text-3xl font-semibold">{user?.name}</h1>
+                    <p className = "text-gray-500 italic">@{user?.username}</p>
                 </div>
                 <div className = "w-full gap-2 flex flex-col">
-                        <Button className = "bg-gray-500" onClick={() => router.push(`/profile/${user.username}/edit`)}>
+                        <Button className = "bg-gray-500" onClick={() => router.push(`/profile/${user?.username}/edit`)}>
                                 <p className = "">Edit Profile</p>
                         </Button>
                     <Button className = "bg-black">
-                        <p className = "">{user.college.name}</p>
+                        <p className = "">{user?.college.name}</p>
                     </Button>
                     <Button>
                         <ContactNow />
@@ -37,7 +37,7 @@ export default function ProfileCard({user}) {
                 <div className = "flex flex-col w-full gap-2">
                     <div className = "font-semibold">Note</div>
                     <div className = "w-full h-[100px] border rounded-lg shadow-md p-1 ">
-                        <p>{user.note}</p>
+                        <p>{user?.note}</p>
                     </div>
                 </div>
             </div>
