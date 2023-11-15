0bd9cd42431f91df44317e5813752b7e2db3145c
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-15 15:23:45 +0530

    Add update phone number field to user profile

--- src/app/api/editUserProfile/route.js
+++ src/app/api/editUserProfile/route.js
@@ -18,6 +18,7 @@ export async function POST(req) {
             data: {
                 name: name,
                 note: note,
+                phoneNo: phoneNo,
                 location: location,
                 college: { connect: { name: college} },
                 branch: { connect: { name: branch} }, 
