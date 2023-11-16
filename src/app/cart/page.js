c40538f8862274254daa97bd3ba20c1e8e247a91
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-16 22:00:04 +0530

    Make card dynamic with info from the db

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,11 +1,25 @@
+'use client'
 import CartCard from "@/components/CartCard";
-import ProductCard from "@/components/ProductCard";
 import { Button } from "@/components/ui/button";
-import BoughtCard from "@/components/BoughtCard"
+import { useSession } from "next-auth/react";
+import { useQuery } from "@tanstack/react-query";
+import { selectCart } from "@/lib/fetchQueries";
 
 export default function Cart(){
+    const session = useSession();
+    let itemsCount = 0;
+    let totalPrice = 0;
+    const cartId = session?.data?.user?.cartId;
+    const cart = useQuery({ 
+        queryKey: ["cart",cartId], 
+        queryFn: () => selectCart(cartId),
+        enabled : !!cartId
+    })
+    if(cart.isLoading){
+        return <div>Loading...</div>
+    }
     return (
-       <main className = "m-5 mb-20">
+        <main className = "m-5 mb-20">
             <div className = "hidden lg:flex flex-col">
                 <div className = "sm:flex flex-row justify-around gap-2">
                     <div className = "sm:flex flex-col gap-5">
@@ -13,18 +27,15 @@ export default function Cart(){
                             <div className = "sm:flex sm:flex-col flex flex-col border rounded-sm shadow-sm p-5 gap-4">
                                 <h2 className = "text-3xl font-bold">Your Cart</h2>
                                 <div className = "sm:flex sm:flex-col gap-3">
-                                    <CartCard /> 
-                                    <CartCard />
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className = "sm:flex sm:flex-col border rounded-sm shadow-sm p-5 gap-4">
-                                <h2 className = "text-3xl font-bold">Previously Bought</h2>
-                                <div className = "sm:flex sm:flex-col gap-3">
-                                    <BoughtCard />
-                                    <BoughtCard /> 
-                                    <BoughtCard />
+                                    {
+                                        cart.data?.products?.map((product) => {
+                                            itemsCount += 1;
+                                            totalPrice += parseInt(product.price);
+                                            return (
+                                                <CartCard product={product} key={product.id}/> 
+                                            )
+                                        })
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -33,11 +44,11 @@ export default function Cart(){
                         <div className = "flex flex-col border rounded shadow gap-2 p-2">
                             <div className = "flex flex-row justify-between gap-6">
                                 <p>Items</p>
-                                <p>6</p>
+                                <p>{itemsCount}</p>
                             </div> 
                             <div className = "flex flex-row justify-between">
                                 <p>Price</p>
-                                <p>₹3000</p>
+                                <p>₹{totalPrice}</p>
                             </div>
                         </div>
                         <Button className = "w-full justify-center">
@@ -46,10 +57,6 @@ export default function Cart(){
                     </div>
                 </div>
             </div>
-
-
-
-
 
             <div className = "lg:hidden flex flex-col gap-3">
                 <div className = "flex flex-col border rounded shadow gap-2 p-2">
@@ -67,26 +74,22 @@ export default function Cart(){
                 </Button>
 
                 <div className = "flex flex-col">
-                            <div className = "sm:flex sm:flex-col border rounded-sm shadow-sm p-5 gap-4">
-                                <h2 className = "text-3xl font-bold text-center m-2">Your Cart</h2>
-                                <div className = "sm:flex sm:flex-col flex flex-col gap-3">
-                                    <CartCard /> 
-                                    <CartCard />
-                                </div>
-                            </div>
+                    <div className = "sm:flex sm:flex-col border rounded-sm shadow-sm p-5 gap-4">
+                        <h2 className = "text-3xl font-bold text-center m-2">Your Cart</h2>
+                        <div className = "sm:flex sm:flex-col flex flex-col gap-3">
+                            {
+                                cart.data?.products?.map((product) => {
+                                    itemsCount += 1;
+                                    totalPrice += parseInt(product.price);
+                                    return (
+                                        <CartCard product={product} key={product.id}/> 
+                                    )
+                                })
+                            }
                         </div>
-                
-                 <div>
-                        <div className = "sm:flex sm:flex-col border rounded-sm shadow-sm p-5 gap-4">
-                            <h2 className = "text-3xl font-bold text-center m-2">Previously Bought</h2>
-                            <div className = "sm:flex sm:flex-col flex flex-col gap-3">
-                                <BoughtCard />
-                                <BoughtCard /> 
-                                <BoughtCard />
-                            </div>
-                        </div>
-                </div> 
+                    </div>
+                </div>
             </div> 
-       </main> 
+        </main> 
     )
 }
