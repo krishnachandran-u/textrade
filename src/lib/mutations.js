4ba2b0bdc3ad6590b7627916bf593bc991bcd64f
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-18 08:31:10 +0530

    Refactor useAddToCartMutation

--- src/lib/mutations.js
+++ src/lib/mutations.js
@@ -3,12 +3,12 @@ import { toast } from '@/components/ui/use-toast';
 import { useCartStore } from '@/lib/stores';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export function useAddToCartMutation() {
+export function useAddToCartMutation(cartId) {
   const [addCartItem] = useCartStore((state) => [state.addItem]);
   const queryClient = useQueryClient();
 
   const addToCartMutation = useMutation({
-    mutationFn: async ({productId,price,cartId}) => {
+    mutationFn: async ({productId,price}) => {
       if(cartId == undefined){
           toast({title: "User session not found", description: "You must be signed in to remove from cart"})
           throw new Error("User session not found")
@@ -22,7 +22,7 @@ export function useAddToCartMutation() {
     },
     onSuccess: (data,variables) => {
       addCartItem(parseInt(variables.price))
-      queryClient.invalidateQueries(["cart",variables.cartId]);
+      queryClient.invalidateQueries(["cart",cartId]);
       toast({title: "Product added to cart", description: "Product added to cart successfully"})
     },
     onError:(error) => {
